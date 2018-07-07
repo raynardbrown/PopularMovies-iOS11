c4258b4ee55991494bd5b189a1ebf2e05df7dbc5
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-07-06 21:20:18 -0500

    Initial commit.

---
+++ PopularMovies/Controller/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//
+//  Created by Raynard Brown on 2018-07-06.
+//  Copyright © 2018 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+}
+
