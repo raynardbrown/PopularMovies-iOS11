44c319200b826c2d502333c93548d4be29215079
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-07-20 17:04:09 -0500

    Added the MovieCollectionViewCell custom cell.

---
+++ PopularMovies/View/Custom Cell/MovieCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCollectionViewCell.swift
+//
+//  Created by Raynard on 2018-07-15.
+//  Copyright © 2018 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell
+{
+  @IBOutlet var movieCollectionImageView: UIImageView!
+  
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+    // Initialization code
+  }
+}
