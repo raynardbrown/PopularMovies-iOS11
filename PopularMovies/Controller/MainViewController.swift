6da312b8ca52f808717af77174cee132718a94d8
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-07-20 18:50:25 -0500

    Added an outlet for the collection view.
    Added the UICollectionViewDelegate and UICollectionViewDataSource protocols.
    
    Added delegate and data source for the collection view.
    
    Added the functions numberOfItemsInSection and cellForItemAt so that we conform to
    the data source protocol.
    
    Registered the movie collection xib file.
    
    Added a handler for movie results that are received asynchronously.
    
    Display movie posters in the collection view cells.

--- PopularMovies/Controller/MainViewController.swift
+++ PopularMovies/Controller/MainViewController.swift
@@ -6,17 +6,59 @@
 //
 
 import UIKit
+import SDWebImage
 
-class MainViewController : UIViewController
+class MainViewController : UIViewController,
+                           UICollectionViewDelegate,
+                           UICollectionViewDataSource
 {
+  @IBOutlet var moviePosterCollectionView: UICollectionView!
+  
+  private var movieListResultObjectArray : [MovieListResultObject] = [MovieListResultObject]()
+  
   override func viewDidLoad()
   {
     super.viewDidLoad()
+    
+    // Set up the delegate and data source for the collection view
+    moviePosterCollectionView.delegate = self
+    moviePosterCollectionView.dataSource = self
+    
+    moviePosterCollectionView.register(UINib(nibName: "MovieCollectionViewCell", bundle: nil),
+                                       forCellWithReuseIdentifier: "customMovieCollectionViewCell")
+    
+    // TODO: Temp code for testing
+    let params : [String : String] = ["api_key" : PopularMoviesConstants.getTheMovieDatabaseApiKey(),
+                                      "page" : "1"]
+    // TODO: Temp code for testing
+    TheMovieDatabaseUtils.queryTheMovieDatabase("https://api.themoviedb.org/3/movie/popular", params, onMovieResults)
   }
 
   override func didReceiveMemoryWarning()
   {
     super.didReceiveMemoryWarning()
   }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+  {
+    return movieListResultObjectArray.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+  {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customMovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
+    
+    let moviePosterPath : String = TheMovieDatabaseUtils.getMoviePosterUriFromPath(movieListResultObjectArray[indexPath.row].getPosterPath())
+    
+    cell.movieCollectionImageView.sd_setImage(with: URL(string: moviePosterPath))
+    
+    return cell
+  }
+  
+  func onMovieResults(movieResults : [MovieListResultObject])
+  {
+    movieListResultObjectArray = movieResults
+    
+    moviePosterCollectionView.reloadData()
+  }
 }
-
