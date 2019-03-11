cedaad1328c9926ee74081a93036120538468680
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-03-10 23:16:31 -0500

    feat: Add class for movie poster Detail View Controller

---
+++ PopularMovies/Controller/MoviePosterDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MoviePosterDetailViewController.swift
+//
+//  Created by Raynard Brown on 2019-03-10.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class MoviePosterDetailViewController : UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+}
