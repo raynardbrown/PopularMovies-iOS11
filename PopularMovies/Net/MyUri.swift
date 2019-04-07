5b6ba9043d3eccd0e4b1e8efa94248d757ca5e1b
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-04-06 23:27:57 -0500

    feat: Implement core functionality

--- PopularMovies/Net/MyUri.swift
+++ PopularMovies/Net/MyUri.swift
@@ -9,15 +9,30 @@ import Foundation
 
 class MyUri
 {
+  enum UriComponents
+  {
+    case SCHEME
+    case AUTHORITY
+    case PATH
+    case QUERY
+    case FRAGMENT
+  }
+  
+  private var url : URL
   private var urlComponents : URLComponents
+  private var queryParameters : [String : String]
   
   init?(_ uriString : String)
   {
     if let url = URL(string: uriString)
     {
+      self.url = url
+      
       if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
       {
         self.urlComponents = urlComponents
+        
+        queryParameters = [:]
       }
       else
       {
@@ -30,30 +45,97 @@ class MyUri
     }
   }
   
+  @discardableResult
   func appendPath(_ path : String) -> Self
   {
-    // TODO: Implement
+    url = url.appendingPathComponent(path)
+    
+    // update the urlComponents
+    if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+    {
+      self.urlComponents = urlComponents
+    }
     
     return self
   }
   
+  @discardableResult
   func appendQueryParameter(_ parameter : String, _ value : String) -> Self
   {
-    // TOOD: Implement
+    queryParameters[parameter] = value
     
     return self
+  }
+  
+  func getComponentsEndingAt(_ uriComponents : UriComponents) -> String
+  {
+    var uriString : String = ""
+
+    if let temp = urlComponents.scheme
+    {
+      uriString.append(temp)
+    }
+
+    if(uriComponents == UriComponents.SCHEME)
+    {
+      return uriString
+    }
+
+    if let temp = urlComponents.host
+    {
+      uriString.append("://\(temp)")
+    }
+
+    if(uriComponents == UriComponents.AUTHORITY)
+    {
+      return uriString
+    }
+
+    uriString.append("\(urlComponents.path)")
+
+    if(uriComponents == UriComponents.PATH)
+    {
+      return uriString
+    }
+    
+    buildQueryItemObjects()
+
+    if let temp = urlComponents.query
+    {
+      uriString.append("?\(temp)")
+    }
+    
+    if(uriComponents == UriComponents.QUERY)
+    {
+      return uriString
+    }
+    
+    if let temp = urlComponents.fragment
+    {
+      uriString.append("#\(temp)")
+    }
+    
+    return uriString
   }
   
   func getPathSegments() -> [String]
   {
-    // TODO: Implement
-    return []
+    return url.pathComponents
   }
   
   func getQueryParameters() -> [String : String]
   {
-    // TODO: Implement
-    return [:]
+    return queryParameters
   }
   
+  private func buildQueryItemObjects()
+  {
+    var queryItems : [URLQueryItem] = []
+    for queryPairs in queryParameters
+    {
+      queryItems.append(URLQueryItem(name: queryPairs.key, value: queryPairs.value))
+    }
+    
+    urlComponents.queryItems = queryItems
+  }
 }
