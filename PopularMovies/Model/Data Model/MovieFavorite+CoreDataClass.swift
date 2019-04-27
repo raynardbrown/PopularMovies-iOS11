897306de29a011bc0dc6a39969a3def8c4491dfc
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-04-26 23:32:47 -0500

    feat: Add the movie favorite entity class

---
+++ PopularMovies/Model/Data Model/MovieFavorite+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  MovieFavorite+CoreDataClass.swift
+//
+//  Created by Raynard Brown on 2019-04-26.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class MovieFavorite : NSManagedObject
+{
+
+}
