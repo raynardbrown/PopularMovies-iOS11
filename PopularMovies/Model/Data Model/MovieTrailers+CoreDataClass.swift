cbe4680fbc5155d34104cceeda752495e914ede5
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-04-28 23:44:16 -0500

    feat: Add the movie trailers entity class

---
+++ PopularMovies/Model/Data Model/MovieTrailers+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  MovieTrailers+CoreDataClass.swift
+//
+//  Created by Raynard Brown on 2019-04-28.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class MovieTrailers : NSManagedObject
+{
+
+}
