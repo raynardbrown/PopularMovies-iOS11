560f67b99c182c3932b2135c07544cb7ca7637da
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-04-29 23:45:10 -0500

    feat: Add the movie review entity class

---
+++ PopularMovies/Model/Data Model/MovieReviews+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  MovieReviews+CoreDataClass.swift
+//
+//  Created by Raynard Brown on 2019-04-29.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class MovieReviews : NSManagedObject
+{
+
+}
