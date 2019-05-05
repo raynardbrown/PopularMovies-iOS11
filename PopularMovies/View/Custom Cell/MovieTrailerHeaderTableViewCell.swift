b970b2c66563beabfef61879fdae0f12401f89af
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-05-04 23:08:48 -0500

    feat: Add movie trailer header cell

---
+++ PopularMovies/View/Custom Cell/MovieTrailerHeaderTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTrailerHeaderTableViewCell.swift
+//
+//  Created by Raynard Brown on 2019-05-04.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class MovieTrailerHeaderTableViewCell : UITableViewCell
+{
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+  }
+}
