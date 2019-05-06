dff8ce92b19238bf6ef15ab5b4852b0e42a3fbed
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-05-05 23:38:01 -0500

    feat: Add reviews header cell

---
+++ PopularMovies/View/Custom Cell/MovieReviewHeaderTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieReviewHeaderTableViewCell.swift
+//
+//  Created by Raynard Brown on 2019-05-05.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class MovieReviewHeaderTableViewCell : UITableViewCell
+{
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+  }
+}
