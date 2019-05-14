67a64ddca7fe8c69f07c718049c4d4bc3d708c8f
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-05-13 20:55:16 -0500

    feat: Add cell for handling empty trailer/reviews

---
+++ PopularMovies/View/Custom Cell/EmptyDetailViewTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EmptyDetailViewTableViewCell.swift
+//
+//  Created by Raynard Brown on 2019-05-09.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class EmptyDetailViewTableViewCell : UITableViewCell
+{
+  @IBOutlet var emptyCellLabel: UILabel!
+  
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+    // Initialization code
+  }
+}
