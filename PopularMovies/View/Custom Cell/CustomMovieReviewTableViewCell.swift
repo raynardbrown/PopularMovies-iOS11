c1903cd675fbb1318e4c40d392f6ab4323bb59ab
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-05-13 23:54:24 -0500

    feat: Add a custom cell for reviews

---
+++ PopularMovies/View/Custom Cell/CustomMovieReviewTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomMovieReviewTableViewCell.swift
+//
+//  Created by Raynard Brown on 2019-05-13.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class CustomMovieReviewTableViewCell : UITableViewCell
+{
+  @IBOutlet var reviewText: UILabel!
+
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+    // Initialization code
+  }
+}
