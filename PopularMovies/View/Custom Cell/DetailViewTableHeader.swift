2f25af89677d791dbeb1819befffb620c2ae39b1
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-05-13 20:52:35 -0500

    feat: Add generic detail view header cell

---
+++ PopularMovies/View/Custom Cell/DetailViewTableHeader.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewTableHeader.swift
+//
+//  Created by Raynard Brown on 2019-05-09.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewTableHeader : UITableViewHeaderFooterView
+{
+  @IBOutlet var headerLabel: UILabel!
+}
