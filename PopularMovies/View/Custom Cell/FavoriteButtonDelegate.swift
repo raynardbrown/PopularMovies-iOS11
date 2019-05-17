60c2406f3bf408ea3092fe7762eb30d38996405c
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-05-16 22:57:01 -0500

    feat: Add a delegate for the favorite button

---
+++ PopularMovies/View/Custom Cell/FavoriteButtonDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteButtonDelegate.swift
+//  PopularMovies
+//
+//  Created by Developer on 5/16/19.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import Foundation
+
+protocol FavoriteButtonDelegate
+{
+  func onFavoriteButtonClicked(_ sender: Any)
+}
