b3a5d23ec2045ec0d085e90ed098f5a887da992a
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-06-02 17:47:00 -0500

    feat: Add a custom cell for an empty favorite DB

---
+++ PopularMovies/View/Custom Cell/NoFavoritesCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  NoFavoritesCollectionViewCell.swift
+//
+//  Created by Raynard Brown on 2019-06-02.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class NoFavoritesCollectionViewCell : UICollectionViewCell,
+                                      Reusable,
+                                      ReusableNib
+{
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+    // Initialization code
+  }
+}
