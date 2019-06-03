b23364fff16024564fffeb44e608ae8c6143e6b2
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-06-02 23:20:01 -0500

    feat: Add NoNetworkConnectionCollectionViewCell

---
+++ PopularMovies/View/Custom Cell/NoNetworkConnectionCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  NoNetworkConnectionCollectionViewCell.swift
+//
+//  Created by Raynard Brown on 2019-06-02.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class NoNetworkConnectionCollectionViewCell : UICollectionViewCell,
+                                              Reusable,
+                                              ReusableNib
+{
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+    // Initialization code
+  }
+}
