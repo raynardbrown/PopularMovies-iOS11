e7106aa86bb270864251852932a317ca3719b305
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-06-27 23:52:27 -0500

    feat: Add NetworkStateMonitor class stub

---
+++ PopularMovies/Net/NetworkStateMonitor.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkStateMonitor.swift
+//
+//  Created by Raynard Brown on 2019-06-27.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import Foundation
+
+class NetworkStateMonitor
+{
+  
+}
