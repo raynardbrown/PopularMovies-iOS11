29519d645e93e2637d3ef830a2afca7995d81930
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-06-29 23:52:36 -0500

    feat: Implement NetworkStateMonitor

--- PopularMovies/Net/NetworkStateMonitor.swift
+++ PopularMovies/Net/NetworkStateMonitor.swift
@@ -5,9 +5,45 @@
 //  Copyright © 2019 Raynard Brown. All rights reserved.
 //
 
-import Foundation
+import Reachability
 
 class NetworkStateMonitor
 {
+  private var networkStateDelegate : NetworkStateDelegate
   
+  private var reachability : Reachability?
+  
+  init(_ networkStateDelegate : NetworkStateDelegate)
+  {
+    self.networkStateDelegate = networkStateDelegate
+    
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(NetworkStateMonitor.networkStateChange),
+                                           name: NSNotification.Name.reachabilityChanged,
+                                           object: nil)
+    
+    self.reachability = Reachability.forInternetConnection()
+    
+    self.reachability!.reachableOnWWAN = false
+    
+    reachability!.startNotifier()
+    
+    // strangely this must be called otherwise there isn't an initial notification
+    NotificationCenter.default.post(name: NSNotification.Name.reachabilityChanged, object: nil)
+  }
+  
+  @objc
+  func networkStateChange() -> Void
+  {
+    if reachability!.isReachableViaWiFi() || reachability!.isReachableViaWWAN()
+    {
+      // network available
+      networkStateDelegate.onNetworkConnected()
+    }
+    else
+    {
+      // network unavailable
+      networkStateDelegate.onNetworkDisconnected()
+    }
+  }
 }
