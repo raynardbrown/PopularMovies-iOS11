a80138b25de1766d759f45de170089cffef61640
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-07-16 22:24:47 -0500

    feat: Add App Info View Controller stub

---
+++ PopularMovies/Controller/AppInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AppInfoViewController.swift
+//
+//  Created by Raynard Brown on 2019-07-16.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class AppInfoViewController : UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+}
