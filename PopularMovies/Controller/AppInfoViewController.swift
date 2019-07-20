2794cd03f89742960f4a3c12b9114e29d478b665
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-07-19 23:50:24 -0500

    feat: Animate and style the app info pop view
    This change adds following:
    
    Adds rounded corners to the app info pop up view.
    
    Lowers the opacity of the view controller so that its parent view
    controller is visible below.
    
    Adds a vertical animation to the app info pop up view.

--- PopularMovies/Controller/AppInfoViewController.swift
+++ PopularMovies/Controller/AppInfoViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class AppInfoViewController : UIViewController
 {
+  @IBOutlet var appInfoView: UIView!
   override func viewDidLoad()
   {
     super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(_ animated: Bool)
+  {
+    // add rounded corners to the app info pop up view
+    appInfoView.layer.cornerRadius = 15
+    
+    // lower the opacity of the background color (we use black as a base color and not the view
+    // original color of the view controller) for the view controller so that its parent
+    // view controller is visible below
+    self.view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+    
+    // simple animation that moves the app info up view vertically
+    appInfoView.alpha = 0
+    self.appInfoView.frame.origin.y = self.appInfoView.frame.origin.y + 50
+    UIView.animate(withDuration: 0.4, animations: { () -> Void in
+      self.appInfoView.alpha = 1.0;
+      self.appInfoView.frame.origin.y = self.appInfoView.frame.origin.y - 50
+    })
   }
 
   override func didReceiveMemoryWarning()
