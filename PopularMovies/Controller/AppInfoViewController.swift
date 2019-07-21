82189b8cfff0698db822121ad8d1d83541d2aa4a
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-07-20 23:51:48 -0500

    docs: Document the layout for the content view

--- PopularMovies/Controller/AppInfoViewController.swift
+++ PopularMovies/Controller/AppInfoViewController.swift
@@ -7,6 +7,19 @@
 
 import UIKit
 
+// Layout information
+//
+// App Info Content View - The Content View for this view controller that sits right below the root
+// view. This view has the following layout properties
+//
+// Centered within the root view.
+// Width set to 300
+// Top margin    >= 16
+// Bottom margin >= 16
+//
+// Note: These two margins are either really large or at least 16 which effectively shrinks the
+// content view vertically if its child views don't need the extra space.
+
 class AppInfoViewController : UIViewController
 {
   @IBOutlet var appInfoView: UIView!
