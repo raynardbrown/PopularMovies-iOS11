544c084ccfd3a6050f671127ee520784bc8ef3bc
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-07-27 23:34:19 -0500

    feat: Add UIBarButtonItemEx class

---
+++ PopularMovies/View/UIKitExtras/UIBarButtonItemEx.swift
@@ -0,0 +1,22 @@
+//
+//  UIBarButtonItemEx.swift
+//
+//  Created by Raynard Brown on 2019-07-27.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class UIBarButtonItemEx
+{
+  private init()
+  {
+    
+  }
+  
+  static func setVisible(_ navItem : UINavigationItem,
+                         _ barButtonItem : UIBarButtonItem)
+  {
+    
+  }
+}
