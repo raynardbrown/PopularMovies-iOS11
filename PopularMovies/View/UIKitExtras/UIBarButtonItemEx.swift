d6cc64ae663fae626f03fd335046671d4b6fb3b5
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-07-31 03:20:09 -0500

    feat: Implement the setVisible function

--- PopularMovies/View/UIKitExtras/UIBarButtonItemEx.swift
+++ PopularMovies/View/UIKitExtras/UIBarButtonItemEx.swift
@@ -25,6 +25,59 @@ class UIBarButtonItemEx
                          _ barButtonLocation : BarButtonLocation,
                          _ visible : Bool)
   {
+    var setBarButtonItems : (([UIBarButtonItem]?, Bool) -> Void)! = nil
+    var items : [UIBarButtonItem]? = nil
     
+    if barButtonLocation == .RightSide
+    {
+      setBarButtonItems = navItem.setRightBarButtonItems
+      items = navItem.rightBarButtonItems
+    }
+    else
+    {
+      setBarButtonItems = navItem.setLeftBarButtonItems
+      items = navItem.leftBarButtonItems
+    }
+    
+    if visible
+    {
+      if var items = items
+      {
+        var foundButton = false
+        
+        for i in 0..<items.count
+        {
+          if items[i] == barButtonItem
+          {
+            foundButton = true
+            break
+          }
+        }
+        
+        if !foundButton
+        {
+          // add the button since it's not in the nav bar
+          items.append(barButtonItem)
+          
+          setBarButtonItems(items, true)
+        }
+      }
+    }
+    else
+    {
+      if var items = items
+      {
+        for i in 0..<items.count
+        {
+          if items[i] == barButtonItem
+          {
+            items.remove(at: i)
+            
+            setBarButtonItems(items, true)
+            break
+          }
+        }
+      }
+    }
   }
 }
